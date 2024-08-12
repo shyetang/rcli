1e0260f93f8f78e212b83f5dc269078ff2046db3
shyetang <1101293391@qq.com>
2024-08-12 21:58:24 +0800

    完成csv转json功能

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, rust!");
+use clap::Parser;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use std::{fs, path::Path};
+// rcli csv -i input.csv -o output.json --header -d ','
+#[derive(Debug, Parser)]
+#[command(name="rcli",version,author,about,long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV,or convert csv to other formats")]
+    Csv(CsvOpts),
+}
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short,long,value_parser=verify_input_file)]
+    // 通过value_parser 我们可以预先处理我们的输入
+    input: String,
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    // #[serde(rename = "Name")]
+    name: String,
+    // #[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    // #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let mut reader = Reader::from_path(opts.input)?;
+            let mut ret = Vec::<Player>::with_capacity(128);
+            for result in reader.deserialize() {
+                let record: Player = result?;
+                ret.push(record);
+            }
+            let json = serde_json::to_string_pretty(&ret)?;
+            fs::write(opts.output, json)?;
+        }
+    }
+    Ok(())
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File does not exist")
+    }
 }
